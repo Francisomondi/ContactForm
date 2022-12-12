e69067c845aa86c4734199ebfe3cf578fd60baae
francis omondi <francisomondi17@gmail.com>
2022-12-12 20:53:36 +0300

    front end set up and home route created

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Home from './pages/Home';
+
+
+function App() {
+  return (
+
+    <BrowserRouter>
+      <div className="App">
+        <ToastContainer position="top-center" />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+
+
+
+
+      </div>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
